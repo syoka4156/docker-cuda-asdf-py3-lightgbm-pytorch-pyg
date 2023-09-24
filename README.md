ae78f9e883261923789a6994ed6426e364f9371d
syoka4156 <notebook27182@gmail.com>
2023-09-25 04:31:02 +0900

    add build_docker_container

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ cuda11.7 + asdf + LightGBM + Pytorch + PyG
 ## Build docker image
 
 ```
-cd docker && docker-compose -p project_name up --build
+export PROJ_NM=hoge
+./build_docker_container.sh
 ```
---
+++ build_docker_container.sh
@@ -0,0 +1,20 @@
+if [ -z $PROJ_NM ]; then
+  echo "Set PROJ_NM (e.g. 'export PROJ_NM=test_proj')"
+  exit 1
+fi
+PROJECT=$PROJ_NM
+CONTAINER="${PROJECT}_app_1"
+echo "$0: PROJECT=${PROJECT}"
+echo "$0: CONTAINER=${CONTAINER}"
+
+# Stop and remove the Docker container.
+EXISTING_CONTAINER_ID=`docker ps -aq -f name=${CONTAINER}`
+if [ ! -z "${EXISTING_CONTAINER_ID}" ]; then
+  echo "The container name ${CONTAINER} is already in use" 1>&2
+  echo ${EXISTING_CONTAINER_ID}
+  exit 1
+fi
+
+
+echo "starting build"
+docker-compose -p ${PROJECT} -f ./docker/docker-compose.yaml up --build
