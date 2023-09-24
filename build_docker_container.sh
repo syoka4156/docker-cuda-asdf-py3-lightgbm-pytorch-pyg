1c9e26326d3737fdca98c6e84b8713eab00e58be
syoka4156 <88654010+syoka4156@users.noreply.github.com>
2023-09-25 04:38:49 +0900

    Update build_docker_container.sh

--- build_docker_container.sh
+++ build_docker_container.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+################################################################################
+
+# Set the Docker container name from a project name (first argument).
 if [ -z $PROJ_NM ]; then
   echo "Set PROJ_NM (e.g. 'export PROJ_NM=test_proj')"
   exit 1
@@ -7,7 +12,7 @@ CONTAINER="${PROJECT}_app_1"
 echo "$0: PROJECT=${PROJECT}"
 echo "$0: CONTAINER=${CONTAINER}"
 
-
+# Stop building if the project name is existing.
 EXISTING_CONTAINER_ID=`docker ps -aq -f name=${CONTAINER}`
 if [ ! -z "${EXISTING_CONTAINER_ID}" ]; then
   echo "The container name ${CONTAINER} is already in use" 1>&2
@@ -15,6 +20,11 @@ if [ ! -z "${EXISTING_CONTAINER_ID}" ]; then
   exit 1
 fi
 
+################################################################################
 
+# Build the Docker container.
 echo "starting build"
 docker-compose -p ${PROJECT} -f ./docker/docker-compose.yaml up --build
+
+# References
+# https://github.com/yikeda0124/docker-ros-cuda-py3
